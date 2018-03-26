d09a476711b2339a11d933dc24953511aa8c92c4
Renato Samperio <renatosamperio@gmail.com>
2018-03-26 14:57:12 +0200

    Updated permissions

--- src/js_module/scripts/ros_node_script.js
+++ src/js_module/scripts/ros_node_script.js
@@ -4,4 +4,4 @@ const rosnodejs = require('rosnodejs');
 const custom_messages_module = rosnodejs.require('custom_messages_module').msg;
 const std_msgs  = rosnodejs.require('std_msgs').msg;
 
-console.log("Loaded expected nodes");
+console.log("- Loaded expected nodes");

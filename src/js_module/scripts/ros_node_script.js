2bc54237f157f86758cf2853b487faa5d40bc8b1
Renato Samperio <renatosamperio@gmail.com>
2018-03-26 15:07:14 +0200

    Created custom message

--- src/js_module/scripts/ros_node_script.js
+++ src/js_module/scripts/ros_node_script.js
@@ -5,3 +5,13 @@ const custom_messages_module = rosnodejs.require('custom_messages_module').msg;
 const std_msgs  = rosnodejs.require('std_msgs').msg;
 
 console.log("- Loaded expected nodes");
+
+var msg = new custom_messages_module.myMessage();
+msg.strings = new Array();
+msg.strings.push("hello");
+msg.strings.push("world");
+msg.number = 10;
+msg.header.stamp = rosnodejs.Time.now();
+
+console.log("- Created custom message");
+console.log(msg);

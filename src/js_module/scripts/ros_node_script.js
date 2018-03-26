c059e22df96c3bb5e90e55fa9ba437f9684543cd
Renato Samperio <renatosamperio@gmail.com>
2018-03-26 19:21:52 +0000

    Included message successfully

--- src/js_module/scripts/ros_node_script.js
+++ src/js_module/scripts/ros_node_script.js
@@ -21,6 +21,14 @@ rosnodejs.initNode('my_node', { onTheFly: true })
 .then(() => {
   const custom_messages_module = rosnodejs.require('custom_messages_module').msg;
   const more_custom_msgs = rosnodejs.require('more_custom_msgs').msg;
+
+  var msg = new more_custom_msgs.Hve_Channel_Status();
+  msg.topic = "my_topic";
+  msg.status = true;
+  msg.last_update = rosnodejs.Time.now();
+
+  console.log("- Created more custom messages");
+  console.log(msg);
 });
 
 const more_custom_msgs = rosnodejs.require('more_custom_msgs').msg;
